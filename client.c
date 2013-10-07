bb3c555f16295e7fa58a32d205e63a7f6b1b9090
Reed Allman <rdallman10@gmail.com>
2013-10-06 21:06:32 -0500

    little changes

--- client.c
+++ client.c
@@ -17,14 +17,13 @@ int main(int argc, char *argv[]) {
   int sockfd,n;
   struct sockaddr_in servaddr,cliaddr;
   struct hostent *server;
+  //maybe bigger?
   char sendline[1000];
   bzero(sendline, 1000);
-  //maybe bigger?
 
-  if (argc < 4)
-  {
-      printf("usage: %s server port reqID host...");
-      exit(1);
+  if (argc < 4) {
+    printf("usage: %s server port reqID host...", argv[0]);
+    exit(1);
   }
 
   sockfd=socket(AF_INET,SOCK_DGRAM,0);
@@ -66,12 +65,13 @@ int main(int argc, char *argv[]) {
   }
   sendline[index] = '\0';
 
+  //TML
   sendline[0] = (index >> 8) & 0xFF;
   sendline[1] = index & 0xFF;
   checksum += sendline[0];
   checksum += sendline[1];
 
-  //TODO compute checksum?
+  //TODO compute checksum correctly?
   sendline[2] = ~checksum;
 
   //FIXME temp for seeing what's up
@@ -85,19 +85,16 @@ int main(int argc, char *argv[]) {
   printf("\n");
 
   sendto(sockfd,sendline,index,0,
-                   (struct sockaddr *)&servaddr,sizeof(servaddr));
+      (struct sockaddr *)&servaddr,sizeof(servaddr));
 
   char recvline[index];
   n=recvfrom(sockfd,recvline,index,0,NULL,NULL);
 
   //TODO only if valid
-  printf("Request %d from group %d:\n", recvline[4], recvline[3]);
-  i = 5;
-  arg = 4;
-  while (i < n) {
-    printf("%s: %d.%d.%d.%d\n", argv[arg++],
+  printf("Request %d:\n", recvline[4]);
+  for (i=5, arg=4; i < n; i += 4) {
+    printf("%s:\t %d.%d.%d.%d\n", argv[arg++],
         0xFF & recvline[i], 0xFF & recvline[i+1], 
         0xFF & recvline[i+2], 0xFF & recvline[i+3]);
-    i += 4;
   }
 }
